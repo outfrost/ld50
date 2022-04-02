a28de6b7750ffcc7d48395eb34441e72bb24bf31
outfrost <kotlet.bahn@gmail.com>
2022-04-02 23:02:02 +0200

    Remove copyright notices

--- game/Game.gd
+++ game/Game.gd
@@ -1,9 +1,3 @@
-# Copyright 2021 Outfrost
-# This work is free software. It comes without any warranty, to the extent
-# permitted by applicable law. You can redistribute it and/or modify it under
-# the terms of the Do What The Fuck You Want To Public License, Version 2,
-# as published by Sam Hocevar. See the LICENSE file for more details.
-
 class_name Game
 extends Node
 
