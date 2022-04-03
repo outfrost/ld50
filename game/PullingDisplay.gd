7bdbc983190b38ae0cbabfb319af5e542694510d
Alex Tengu <aglvov@gmail.com>
2022-04-04 00:03:47 +0300

    minor: playing with stats screen (incomplete)

---
+++ game/PullingDisplay.gd
@@ -0,0 +1,28 @@
+extends Control
+
+export(NodePath) var animplayer_path
+onready var animplayer: Node = get_node(animplayer_path)
+
+var display_shown:bool = false
+var display_busy:bool = false
+
+
+func _ready():
+	self.visible = false
+
+func _input(event):
+	if event.is_action_pressed("test_action_a") and !display_shown and !display_busy:
+		self.visible = true
+		display_busy = true
+		display_shown = true
+		animplayer.play("Display-In")
+		yield(get_tree().create_timer(1.0), "timeout")
+		display_busy = false
+
+	if event.is_action_pressed("test_action_b") and display_shown and !display_busy:
+		display_busy = true
+		display_shown = false
+		animplayer.play("Display-Out")
+		yield(get_tree().create_timer(1.0), "timeout")
+		self.visible = false
+		display_busy = false
