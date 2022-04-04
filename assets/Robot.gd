b0c9695d677575ce3cf3d715f28766b1af208026
marekdomagala <45537789+marekdomagala@users.noreply.github.com>
2022-04-04 23:23:49 +0200

    Updated Robot.gd (triggers one event)

--- assets/Robot.gd
+++ assets/Robot.gd
@@ -2,15 +2,21 @@ extends Spatial
 
 onready var anim: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
 
+var robotworking_sound
+
 func _ready() -> void:
 #	Sound.play("Robot", self)
+	robotworking_sound = Sound.instance("RobotWorking").attach(self)
 	pass
 
 func start_working() -> void:
 	anim.travel("robotWorking")
+	robotworking_sound.start()
+	
 
 func stop_working() -> void:
 	anim.travel("robotDeactivate")
+	robotworking_sound.stop()
 
 # Sound.instance("Robot").attach(self).start()
 # Every instance of the Robot event triggers one of the three drill sounds
