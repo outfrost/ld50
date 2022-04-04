02028fc43e3a5726e9e40df640a3cdd8582b5a40
outfrost <kotlet.bahn@gmail.com>
2022-04-04 22:43:50 +0200

    Add auto transitions in robot animation

--- assets/Robot.gd
+++ assets/Robot.gd
@@ -1,2 +1,12 @@
 extends Spatial
 
+onready var anim: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+
+func _ready() -> void:
+	pass
+
+func start_working() -> void:
+	anim.travel("robotWorking")
+
+func stop_working() -> void:
+	anim.travel("robotDeactivate")
