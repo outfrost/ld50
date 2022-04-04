50ef7f13e7f7031ff08715af0dabf167079b69f3
Alex Tengu <aglvov@gmail.com>
2022-04-04 07:28:12 +0300

    working with transitions between levels

--- game/StatsShowcase.gd
+++ game/StatsShowcase.gd
@@ -15,10 +15,21 @@ func _ready():
 	hellorobot.visible = false
 	gameover.visible = false
 
+func _input(event):
+	if event is InputEventKey and is_shown:
+		pass
+
+func _process(delta):
+	if is_shown:
+		pass
+
+
 func shift_stats_screen():
 	# asking loop controller for stats, shows them
 	get_node("ShiftStats/Label").text = "Hello there!"
+	self.visible = true
 	shiftstats.visible = true
+
 
 func gameover_screen():
 	pass
