ccd9f7da96fd4840b64f2fc7a7bab27a213a0d6f
outfrost <kotlet.bahn@gmail.com>
2022-04-07 03:17:43 +0200

    Export script

--- copy_sound_stuff.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -R sound build/linux_amd64/
-cp -R sound build/windows_amd64/
-cp -R sound build/macos/
-
-cd build/linux_amd64/
-ln -f -s libfmod.so libfmod.so.13
-ln -f -s libfmodstudio.so libfmodstudio.so.13
---
+++ export.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Creating build directories" >&2
+mkdir -p build/RePlacement-Linux/
+mkdir -p build/RePlacement-Windows/
+
+echo >&2
+echo "Starting Linux export" >&2
+godot -v --no-window --export RePlacement-Linux-X11-amd64 build/RePlacement-Linux/replacement
+echo >&2
+echo "Starting Windowsexport" >&2
+godot -v --no-window --export RePlacement-Windows-amd64 build/RePlacement-Windows/replacement.exe
+
+echo >&2
+echo "Copying sound files" >&2
+cp -R sound build/RePlacement-Linux/
+rm build/RePlacement-Linux/sound/*.import
+cp -R sound build/RePlacement-Windows/
+rm build/RePlacement-Windows/sound/*.import
+
+echo >&2
+echo "Creating library symlinks" >&2
+pushd build/RePlacement-Linux/ >/dev/null
+ln -f -s libfmod.so libfmod.so.13
+ln -f -s libfmodstudio.so libfmodstudio.so.13
+popd >/dev/null
+
+echo >&2
+echo "Archiving exports" >&2
+pushd build/ >/dev/null
+echo "RePlacement-Linux.zip" >&2
+zip -r RePlacement-Linux.zip RePlacement-Linux
+echo "RePlacement-Windows.zip" >&2
+zip -r RePlacement-Windows.zip RePlacement-Windows
+popd >/dev/null
